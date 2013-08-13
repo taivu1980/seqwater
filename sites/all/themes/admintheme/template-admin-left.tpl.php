56ce84da5983596d6322e8b347e6fd5c25592e60
Phuc Long <phuc@phuc-long.(none)>
2013-08-13 15:07:19 +0700

    left menu admin

--- sites/all/themes/admintheme/template-admin-left.tpl.php
+++ sites/all/themes/admintheme/template-admin-left.tpl.php
@@ -16,26 +16,26 @@
 </div>
 <div class="left-menu-item">
     <ul>
-        <li><a href="/?q=node/add/article">Add Article</a></li>
+        <li><a href="/seqwater/node/add/article">Add Article</a></li>
         <li><a href="#">Add Video</a></li>
         <li><a href="#">Add Picture</a></li>
         <li><a href="#">Add Gallery</a></li>
     </ul>
     <hr/>
     <ul>
-        <li><a href="/?q=admin">Admin page</a></li>
-        <li><a href="/?q=admin/content">Content</a></li>
-        <li><a href="/?q=admin/structure">Structure</a></li>
-        <li><a href="/?q=admin/dashboard">Dashboard</a></li>
-        <li><a href="/?q=admin/help">Help</a></li>
+        <li><a href="/seqwater/admin">Admin page</a></li>
+        <li><a href="/seqwater/admin/content">Content</a></li>
+        <li><a href="/seqwater/admin/structure">Structure</a></li>
+        <li><a href="/seqwater/admin/dashboard">Dashboard</a></li>
+        <li><a href="/seqwater/admin/help">Help</a></li>
     </ul>
     <hr/>
     <ul>
-        <li><a href="/?q=admin/config">Configuration</a></li>
-        <li><a href="/?q=admin/modules">Modules</a></li>
-        <li><a href="/?q=admin/reports">Reports</a></li>
-        <li><a href="/?q=admin/people">People</a></li>
-        <li><a href="/?q=admin/appearance">Appearance</a></li>        
+        <li><a href="/seqwater/admin/config">Configuration</a></li>
+        <li><a href="/seqwater/admin/modules">Modules</a></li>
+        <li><a href="/seqwater/admin/reports">Reports</a></li>
+        <li><a href="/seqwater/admin/people">People</a></li>
+        <li><a href="/seqwater/admin/appearance">Appearance</a></li>        
     </ul>
     <hr/>
 </div>
