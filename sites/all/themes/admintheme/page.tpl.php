b59766f457966ee87f4eb7e12120c14a688d4a87
Phuc Duong <phuc.duong@kiss-concept.com>
2013-08-13 15:15:47 +0700

    left menu admin

--- sites/all/themes/admintheme/page.tpl.php
+++ sites/all/themes/admintheme/page.tpl.php
@@ -11,7 +11,7 @@
         <div class="notification">
             <span class="notification1" title="Waiting approve">(2)</span>
             <span class="notification2" title="News change">(5)</span>
-            <span><a href="/?q=user/logout">Logout</a></span>
+            <span><a href="/seqwater/user/logout">Logout</a></span>
         </div>
         <!-- end - notification-->
         
