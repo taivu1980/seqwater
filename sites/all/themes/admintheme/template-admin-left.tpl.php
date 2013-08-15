952cdb4a5838827fb29ab323194b8e6380e19322
phuc <phuc@localhost>
2013-08-15 08:27:12 +0700

    icon menu left

--- sites/all/themes/admintheme/template-admin-left.tpl.php
+++ sites/all/themes/admintheme/template-admin-left.tpl.php
@@ -16,26 +16,26 @@
 </div>
 <div class="left-menu-item">
     <ul>
-        <li><a href="/seqwater/node/add/article">Add Article</a></li>
-        <li><a href="#">Add Video</a></li>
-        <li><a href="#">Add Picture</a></li>
-        <li><a href="#">Add Gallery</a></li>
+        <li class="add-article"><a title="Add Article" href="/seqwater/node/add/article">Add Article</a></li>
+        <li class="add-video"><a title="Add Video" href="#">Add Video</a></li>
+        <li class="add-picture"><a title="Add Picture" href="#">Add Picture</a></li>
+        <li class="add-gallery"><a title="Add Gallery" href="#">Add Gallery</a></li>
     </ul>
     <hr/>
     <ul>
-        <li><a href="/seqwater/admin">Admin page</a></li>
-        <li><a href="/seqwater/admin/content">Content</a></li>
-        <li><a href="/seqwater/admin/structure">Structure</a></li>
-        <li><a href="/seqwater/admin/dashboard">Dashboard</a></li>
-        <li><a href="/seqwater/admin/help">Help</a></li>
+        <li class="admin-page"><a href="/seqwater/admin">Admin page</a></li>
+        <li class="content"><a href="/seqwater/admin/content">Content</a></li>
+        <li class="structure"><a href="/seqwater/admin/structure">Structure</a></li>
+        <li class="dashboard"><a href="/seqwater/admin/dashboard">Dashboard</a></li>
+        <li class="help"><a href="/seqwater/admin/help">Help</a></li>
     </ul>
     <hr/>
     <ul>
-        <li><a href="/seqwater/admin/config">Configurations</a></li>
-        <li><a href="/seqwater/admin/modules">Modules</a></li>
-        <li><a href="/seqwater/admin/reports">Reports</a></li>
-        <li><a href="/seqwater/admin/people">People</a></li>
-        <li><a href="/seqwater/admin/appearance">Appearance</a></li>        
+        <li class="configuration"><a href="/seqwater/admin/config">Configuration</a></li>
+        <li class="module"><a href="/seqwater/admin/modules">Modules</a></li>
+        <li class="report"><a href="/seqwater/admin/reports">Reports</a></li>
+        <li class="people"><a href="/seqwater/admin/people">People</a></li>
+        <li class="appearance"><a href="/seqwater/admin/appearance">Appearance</a></li>        
     </ul>
     <hr/>
 </div>
