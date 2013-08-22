9bc0ec4db27901e215bdc3ffe84b08245af0cdb6
trivan <trivan@kiss-concept.com>
2013-08-22 13:29:04 +0700

    edit css

--- sites/all/themes/tweme/templates/views-view-unformatted--top-new-articles--block.tpl.php
+++
@@ -1,7 +0,0 @@
-<?php
-// print $rows;
-foreach ($rows as $row){
-	echo"<div class='row-block-news'><div class='live-news-update-button btn-news-customize'>NEWS</div>";
-	print $row;
-	echo"</div>";
-}
